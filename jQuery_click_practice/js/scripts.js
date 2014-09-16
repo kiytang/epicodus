47c38d48b728ddd1cc2b1d4d2baf0708a37f00ab
Kiy Hong Tang <kiyhong@gmail.com>
2014-09-16 13:55:38 +0100

    Changed jQuery to

--- jQuery_click_practice/js/scripts.js
+++ jQuery_click_practice/js/scripts.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
 	jQuery("h1").click(function() {
 	alert("This is a header.");
 	alert("I told you this is a header");
 	});
 
-	jQuery("p").click(function() {
+	$("p").click(function() {
 		alert("This is a paragraph.");
 	});
 
-	jQuery("img").click(function() {
+	$("img").click(function() {
 		alert("This is an image");
 	});
 });
