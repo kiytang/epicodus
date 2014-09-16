2cdd8de7c864f9bd7b6974e880451e4850485b81
Kiy Hong Tang <kiyhong@gmail.com>
2014-09-16 12:24:21 +0100

    Added jQuery(document).ready tag

--- jQuery_click_practice/js/scripts.js
+++ jQuery_click_practice/js/scripts.js
@@ -1,12 +1,14 @@
-jQuery("h1").click(function() {
+jQuery(document).ready(function() {
+	jQuery("h1").click(function() {
 	alert("This is a header.");
 	alert("I told you this is a header");
-});
+	});
 
-jQuery("p").click(function() {
-	alert("This is a paragraph.");
-});
+	jQuery("p").click(function() {
+		alert("This is a paragraph.");
+	});
 
-jQuery("img").click(function() {
-	alert("This is an image");
+	jQuery("img").click(function() {
+		alert("This is an image");
+	});
 });
