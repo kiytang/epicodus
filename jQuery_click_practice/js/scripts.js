c1a2fa8edf6a5cbeeb639787486f9d55c638f943
Kiy Hong Tang <kiyhong@gmail.com>
2014-09-16 12:17:19 +0100

    Added alert function for both <p> and <img> tag

--- jQuery_click_practice/js/scripts.js
+++ jQuery_click_practice/js/scripts.js
@@ -1,3 +1,12 @@
 jQuery("h1").click(function() {
 	alert("This is a header.");
+	alert("I told you this is a header");
+});
+
+jQuery("p").click(function() {
+	alert("This is a paragraph.");
+});
+
+jQuery("img").click(function() {
+	alert("This is an image");
 });
