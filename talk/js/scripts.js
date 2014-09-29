54120058ff0f4cefbddec9bfeb0554dd53b49073
Kiy Hong Tang <kiyhong@gmail.com>
2014-09-29 11:16:50 +0100

    Completed task of adding (this).remove() to the Dom

--- talk/js/scripts.js
+++ talk/js/scripts.js
@@ -4,17 +4,37 @@ $(document).ready(function() {
 		$("ul#webpage").prepend("<li>Why hello there!</li>");
 
 		$("ul#user").children("li").first().click(function() {
-			alert('hi');
+			$(this).remove();
 		});
+
+		$("ul#webpage").children("li").first().click(function() {
+			$(this).remove();
+		})
 	});
 
 	$("button#goodbye").click(function() {
 		$("ul#user").prepend("<li>Goodbye!</li>");
 		$("ul#webpage").prepend("<li>Goodbye dear user!</li>");
+
+		$("ul#user").children("li").first().click(function() {
+			$(this).remove();
+		});
+
+		$("ul#webpage").children("li").first().click(function() {
+			$(this).remove();
+		})
 	});
 
 	$("button#stop").click(function() {
 		$("ul#user").prepend("<li>Stop copying me!</li>");
 		$("ul#webpage").prepend("<li>Stop, Stop Stop!!!</li>");
+
+		$("ul#user").children("li").first().click(function() {
+			$(this).remove();
+		});
+
+		$("ul#webpage").children("li").first().click(function() {
+			$(this).remove();
+		});
 	});
 })
