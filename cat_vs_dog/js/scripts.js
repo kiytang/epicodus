2bae6508f5dcc5a62279bbefe721c9712ec5c5ae
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-01 09:57:24 +0100

    Completed delete functionality

--- cat_vs_dog/js/scripts.js
+++ cat_vs_dog/js/scripts.js
@@ -2,10 +2,28 @@ $(document).ready(function() {
 	$("button#cat").click(function() {
 		$("ul#starter").prepend("<li>meow <span class='clickable'>[x]</span></li>");
 		$("ul#response").prepend("<li>bark bark <span class='clickable'>[x]</span></li>");
+
+		$("ul#starter").children("li").first().click(function() {
+			$(this).remove();
+		})
+		$("ul#response").children("li").first().click(function() {
+			$(this).remove();
+		})
 	});
 
 	$("button#dog").click(function() {
 		$("ul#starter").prepend("<li>bark bark <span class='clickable'>[x]</span></li>");
 		$("ul#response").prepend("<li> meow meow <span class='clickable'>[x]</span></li>");
+
+		$("ul#starter").children("li").first().click(function() {
+			$(this).remove();
+		})
+		$("ul#response").children('li').first().click(function() {
+			$(this).remove();
+		})
+
+
 	});	
+
+
 });
