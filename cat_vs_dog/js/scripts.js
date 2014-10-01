03197c5aa076d964a61052f7df8724af90e09587
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-01 09:40:08 +0100

    Added span tag to button#dog

--- cat_vs_dog/js/scripts.js
+++ cat_vs_dog/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 	});
 
 	$("button#dog").click(function() {
-		$("ul#starter").prepend("<li>bark bark</li>");
-		$("ul#response").prepend("<li> meow meow</li>");
+		$("ul#starter").prepend("<li>bark bark <span class='clickable'>[x]</span></li>");
+		$("ul#response").prepend("<li> meow meow <span class='clickable'>[x]</span></li>");
 	});	
 });
