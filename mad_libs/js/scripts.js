20382f5cbbf67348ad52921f4c938ebcd7db1ab0
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-06 10:59:18 +0100

    Added jQuery event.preventDefault() Method

--- mad_libs/js/scripts.js
+++ mad_libs/js/scripts.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-	$('#blanks form').submit(function() {
+	$('#blanks form').submit(function(event) {
 		$('.person1').append("blah blah");
 		$('.person2').append("blah blah");
 		$('.animal').append("blah blah");
 		$('.exclaimation').append("blah blah");
 		$('.verb').append("blah blah");
-		$('.noun').append("blah blah")
+		$('.noun').append("blah blah");
 
 		$('#story').show();
-	})
-})
+
+		event.preventDefault();
+	});
+});
