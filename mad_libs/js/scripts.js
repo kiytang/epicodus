ca86ee8ff6b1fa200497fdd294eec2269946db39
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-06 11:17:45 +0100

    Added various variable names

--- mad_libs/js/scripts.js
+++ mad_libs/js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 	$('#blanks form').submit(function(event) {
+		var person1Input = $("input#person1").val();
+		var person2Input = $("input#person2").val();
+		var animalInput  = $("input#animal").val();
+		var exclaimationInput = $("input#exclaimation").val();
+		var verbInput = $("input#verb").val();
+		var nounInput = $("input#noun").val();
+
+
 		$('.person1').append("blah blah");
 		$('.person2').append("blah blah");
 		$('.animal').append("blah blah");
