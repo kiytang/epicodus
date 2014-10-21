487ac10d0cf880080e6c26c79dec1b669976d573
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-21 14:29:04 +0100

    Added removeClass

--- background/js/scripts.js
+++ background/js/scripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$("button#first").click(function() {
+	$("body").removeClass();
 	$("body").addClass("change-background");
 	});
 
 	$("button#second").click(function() {
+	$("body").removeClass();
 	$("body").addClass("original-background");
 	});
 })
