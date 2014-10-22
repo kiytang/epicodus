e70bd85127439a71c678dc04be13b60bf4e4cc58
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-22 10:58:49 +0100

    Added method .animate

--- background/js/scripts.js
+++ background/js/scripts.js
@@ -8,4 +8,11 @@ $(document).ready(function() {
 	$("body").removeClass();
 	$("body").addClass("original-background");
 	});
+
+	$("#btn1").click(function() {
+		$("#rectangle").animate({height: "600px"});
+	});
+	$("#btn2").click(function() {
+		$("#rectangle").animate({height: "100px"});
+	});
 })
