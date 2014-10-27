5e3b8d72ebbaa07c7dc287f37a7948e0576e2d66
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-27 17:02:00 +0000

    Added JS for the animal snake

--- animal_branching/js/scripts.js
+++ animal_branching/js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-	var animal = prompt("For more information, please type 'insect', 'turtle' or 'snake'");
+	var animal = prompt("For more information, please type either 'insect', 'turtle' or 'snake'");
 
 	if (animal === 'insect') {
 		$('#insect').show();
 	} else if (animal === 'turtle') {
 		$('#turtle').show();
-	} else {
+	} else if (animal === 'snake') {
 		$('#snake').show();
+	} else {
+		alert('Unreognised request, please try again');
 	};
 });
