dd4664b86c8eb494e2a2732175335ce3ffd6f7c7
Kiy Hong Tang <kiyhong@gmail.com>
2014-10-30 11:09:26 +0000

    Added js to cater for under 17's

--- car_insuarance/js/scripts.js
+++ car_insuarance/js/scripts.js
@@ -3,13 +3,22 @@ $(document).ready(function() {
 		var age = parseInt($("input#age").val());
 		var gender = $("select#gender").val();
 
-		var quote = (100-age)*3;
-		if (gender === 'male' && age < 26) {
-			quote += 50;
-		}
-		$("#rate").empty().append(quote);
-		$("#quote").show();
+		if (age < 17) {
+			alert('Under 17');
+			$('#under-17').show();
+			
+		} else 
+		if (age) {
+			var quote = (100-age)*3;
+			if (gender === 'male' && age < 26) {
+				quote += 50;
+			}
 
+			$("#rate").empty().append(quote);
+			$("#quote").show();
+		} else {
+			alert('Please enter your age.');
+		}	
 		event.preventDefault();
 	});
 });
