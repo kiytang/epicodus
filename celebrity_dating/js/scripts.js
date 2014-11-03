c55ef587ead9a1ae18d1a7fe5fdfa272dda9a127
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-03 20:51:56 +0800

    Added female and male in js

--- celebrity_dating/js/scripts.js
+++ celebrity_dating/js/scripts.js
@@ -2,5 +2,21 @@ $(document).ready(function() {
 	$("form#date").submit(function(event) {
 		var age = parseInt($("input#age").val());
 		var gender = $("select#gender").val();
+
+		var celebrity_1 = "Mickey Mouse"
+		var celebrity_2 = "Miss Piggy"
+
+		if (gender === 'male') {
+			person = person_2;
+		}
+
+		elsif (gender == 'female') {
+			person === person_1;
+		}
+
+		else if (age < 17 {
+			alert('Under 17');
+			$('$under-17').show();
+		})
 	})
 })
