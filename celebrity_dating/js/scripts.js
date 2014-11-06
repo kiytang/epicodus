62542f2c7d025adde41105e1f39434f27197da92
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-07 00:26:47 +0800

    Added var prediction

--- celebrity_dating/js/scripts.js
+++ celebrity_dating/js/scripts.js
@@ -5,26 +5,27 @@ $(document).ready(function() {
 
 		var celebrity_1 = "Mickey Mouse";
 		var celebrity_2 = "Miss Piggy";
+		var prediction = "";
 
-		if (gender === 'male') {
-			date = celebrity_2;
+		if (gender === "Male") {
+			prediction = celebrity_2;
 		}
 
-		else if (gender == 'female') {
-			date === celebrity_1;
+		else if (gender === "Female") {
+			prediction = celebrity_1;
 		}
 
 		else if (age < 17) {
 			alert('Under 17');
-			$('$too-young').show();
+			$("#too-young").show();
 		}
 
 		else if (age > 30) {
 			alert('Over 30');
-			$('#too-old').show();
+			$("#too-old").show();
 		}
 
-		$("#celebrity").empty().append(date);
+		$("#celebrity").empty().append(prediction);
 		$("#result").show();
 		
 		} else {
