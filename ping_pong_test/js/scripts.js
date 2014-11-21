4617ce6a3d10a3ef889542faf7957d93777cd241
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-21 17:13:24 +0000

    Added  else if and else statements

--- ping_pong_test/js/scripts.js
+++ ping_pong_test/js/scripts.js
@@ -1,10 +1,19 @@
 jQuery(document).ready(function($) {
 	var number = prompt('What number would you like me to ping-pong up to?');
 
-	for (var i=1; i<=number; i++) {
+	for (var i = 1; i <= number; i++) {
 		if (i % 15 === 0) {
 			$('.row').append('<li>Ping-Pong</li>');
-		} 
+		} else if (i % 5 === 0) {
+			$('.row').append('<li>Pong</li>');
+		} else if (i % 3 === 0) {
+			$('.row').append('<li>Ping</li>');
+		}	else {
+			$('.row').append('<li>' + i + '</li>');
+		}
+	}
+});
+		}
 		}
 	}
 
