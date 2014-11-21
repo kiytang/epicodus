48ff2fafe060da7b7295f325086de2a200fc788b
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-21 16:51:49 +0000

    Added if statement

--- ping_pong_test/js/scripts.js
+++ ping_pong_test/js/scripts.js
@@ -1,6 +1,12 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function($) {
 	var number = prompt('What number would you like me to ping-pong up to?');
-	
+
+	for (var i=1; i<=number; i++) {
+		if (i % 15 === 0) {
+			$('.row').append('<li>Ping-Pong</li>');
+		} 
+		}
+	}
 
 
 })
