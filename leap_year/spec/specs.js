31d4b3ff7ee9b0ff6594a1a160cfe3846f376485
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-25 10:07:24 +0000

    Added second spec

--- leap_year/spec/specs.js
+++ leap_year/spec/specs.js
@@ -2,4 +2,8 @@ describe('leapYear', function() {
 	it('is false for a year that is not divisible by 4', function() {
 		leapYear(1999).should.equal(false);
 	});
+
+	it('is true for most years divisible by 4', function() {
+		leapYear(2012).should.equal(true);
+	});
 });
