e573c3013a0c59848f9ae986b072210ea2b72344
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-25 10:45:06 +0000

    1st and 2nd spec passing

--- leap_year/spec/specs.js
+++ leap_year/spec/specs.js
@@ -1,9 +1,9 @@
 describe('leapYear', function() {
-	it('is false for a year that is not divisible by 4', function() {
-		leapYear(1999).should.equal(false);
-	});
+  it("is false for a year that is not divisible by 4", function() {
+    leapYear(1999).should.equal(false);
+  });
 
-	it('is true for most years divisible by 4', function() {
-		leapYear(2012).should.equal(true);
-	});
+  it("is true for most years divisible by 4", function() {
+  	leapYear(2012).should.equal(true);
+  });
 });
