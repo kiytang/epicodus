f8746e99f1794e5186791dc51b9f8a04e4b5dbdb
Kiy Hong Tang <kiyhong@gmail.com>
2014-11-27 17:01:41 +0000

    Added test for years divisible by 400

--- leap_year/js/scripts.js
+++ leap_year/js/scripts.js
@@ -1,7 +1,10 @@
 var leapYear = function(year) { 
 	if ((year % 4 === 0) && (year % 100 !== 0)) {
 		return true;
-  } else {
+  } else if
+  		(year % 400 === 0)
+  	return true;
+  	else {
   	return false;
   };
 };
--- leap_year/spec/specs.js
+++ leap_year/spec/specs.js
@@ -10,4 +10,8 @@ describe('leapYear', function() {
   it("is false for most years divisible by 100", function() {
   	leapYear(1900).should.equal(false);
   });
+  // There's one final rule for leap years: if a year is divisible by 400, it is a leap year. Write a test, make it pass, then refactor your code.
+  it("is true for most years divisible by 400", function() {
+  	leapYear(2000).should.equal(true);
+  });
 });
